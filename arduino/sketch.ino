1164c26b305f8ecca788714daae593f0c958089d
styts <github@styts.com>
2012-11-18 10:32:41 +0100

    added yesterdays' sketch and serial reading program

---
+++ arduino/sketch.ino
@@ -0,0 +1,22 @@
+const int xpin = A0;                  // x-axis of the accelerometer
+const int ypin = A1;                  // y-axis
+const int zpin = A2;                  // z-axis (only on 3-axis models)
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.print(analogRead(xpin));
+  // print a tab between values:
+  Serial.print("\t");
+  Serial.print(analogRead(ypin));
+  // print a tab between values:
+  Serial.print("\t");
+  Serial.print(analogRead(zpin));
+  Serial.print("\n");
+  // delay before next reading:
+  delay(100);
+}
