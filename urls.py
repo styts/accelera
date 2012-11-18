08369bdba7adedcaefeb604aef4be69bc681757c
styts <github@styts.com>
2012-11-18 11:09:16 +0100

    hello world home view

--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'accelera.views.home', name='home'),
+    url(r'^$', 'accelera.views.home', name='home'),
     # url(r'^accelera/', include('accelera.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
